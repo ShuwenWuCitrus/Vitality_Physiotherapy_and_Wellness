7d77358574da39aacc727fda9aaeea852c033dd8
Ernestodavilacardona <davi1090@algonquinlive.com>
2024-08-05 23:12:48 -0400

    Modified blade displaying services

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,12 +45,12 @@
                                 class="w-[100px] h-[100px] pb-3" alt="">
                         </div>
                         <div>
-                        <ul>
+                        <ul class="text-sm">
                             @foreach ($professional['services'] as $service)
                                 <li>{{ $service['name'] }}</li>
                             @endforeach
                         </ul>
-                            <p><!-- Here we need to get the service each professional has.--></p>
+                            <p><!-- Here we need to get the service each professional has.-->XX</p>
                         </div>
                     </div>
                 @endforeach
@@ -59,29 +59,7 @@
         </div>
     </div>
 </section>
-<section id="professionals" class="pt-[40px] pb-[100px]">
-    <div class="flex flex-col items-center justify-center text-[#222222]">
-        <div class="pb-8">
-            <h1 class="text-[24px] font-[900]">Professionals</h1>
-        </div>
-        <div class="grid grid-cols-5 grid-rows-2 font-[800] gap-y-16">
-            @foreach ($professionals as $professional)
-            <div class="flex flex-col items-center justify-center">
-                <div>
-                    <p class="py-3"> {{ $professional['name'] }} </p>
-                </div>
-                <div>
-                    <img src="{{ asset('images/' . $professional['name'] . '.webp') }}" class="w-[100px] h-[100px] pb-3" alt="">
-                </div>
-                <div>
-                    <p><!-- Here we need to get the service each professional offers--></p>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
-    </div>
-</section>
+
 
 
 @endsection
